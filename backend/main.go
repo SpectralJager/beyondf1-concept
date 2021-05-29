bd635ba9e270f1eab0ac23c4d40f339cf78a144e
SpectralJager <unkn.spectrum@gmail.com>
2021-05-30 01:03:51 +0700

    init

--- backend/main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	// create server
-	CreateServer()
-}
--- frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import CKEditor from '@ckeditor/ckeditor5-vue';
-
-createApp(App).use(router).use( CKEditor ).mount('#app')
