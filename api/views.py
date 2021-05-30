cfd7f20b61a7f1cb9aac2f918f16de6f417e59b3
SpectralJager <unkn.spectrum@gmail.com>
2021-05-30 17:25:56 +0700

    update

--- api/views.py
+++ api/views.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers, viewsets
+from rest_framework.pagination import PageNumberPagination
 from .models import Article, Subscribers
 from .serializers import ArticleSerializer, SubscribersSerializer
-# Create your views here.
 
+
+#pagination class
+class HomePagePagination(PageNumberPagination):
+    page_size = 8
+    page_query_param = "page_size"
+    max_page_size = 80
+
+# Create your views here.
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all().order_by('-published_date')
     serializer_class = ArticleSerializer
+    pagination_class = HomePagePagination
     http_method_names = ['get']
 
 class SubscribersViewSet(viewsets.ModelViewSet):
